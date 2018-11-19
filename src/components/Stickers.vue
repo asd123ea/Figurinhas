c15d7b12dfc91053efc40e4091009261d90a09ab
Bruno Galvao <bgalvao.work2@gmail.com>
2018-11-19 00:17:01 -0200

    Retirei os detalhes dos sticker da tela de stickers e vou colocar na tela de sticker(detalhe de cada sticker)

--- src/components/Stickers.vue
+++ src/components/Stickers.vue
@@ -44,22 +44,14 @@ export default {
     methods: {
         defineMocks() {
             if(!this.stickers.length) {
-                this.stickers.push({ number: 50, name: 'Ramiro Funes Mori',
-                    total: 11, img: 'https://www.gletech.com/StockPhotos/Soccer/2018/134010/front_6512026.jpg' })
-                this.stickers.push({ number: 51, name: 'Yahya Al-Shehri', 
-                    total: 15, img: 'https://images-na.ssl-images-amazon.com/images/I/51PSz44ItzL._SY445_.jpg' })
-                this.stickers.push({ number: 52, name: 'Paolo Guerrero', 
-                    total: 81, img: 'https://images-na.ssl-images-amazon.com/images/I/51V%2BMpz0wbL._SY445_.jpg' })
-                this.stickers.push({ number: 53, name: 'Lionel Messi', 
-                    total: 61, img: 'https://i.ebayimg.com/images/g/olEAAOSwACRapeGm/s-l300.jpg' })
-                this.stickers.push({ number: 54, name: 'James Rodriguez', 
-                    total: 31, img: 'https://www.coleka.com/media/item/201804/08/fifa-world-cup-russia-2018-james-rodriguez-colombia-643.jpg' })
-                this.stickers.push({ number: 61, name: 'Manuel Neuer', 
-                    total: 25, img: 'https://i.pinimg.com/originals/6b/5a/25/6b5a256be855a8230dc1269927ce8897.jpg' })
-                this.stickers.push({ number: 62, name: 'Cristiano Ronaldo', 
-                    total: 66, img: 'https://www.coleka.com/media/item/201804/02/fifa-world-cup-russia-2018-cristiano-ronaldo-portugal-130.jpg' })
-                this.stickers.push({ number: 69, name: 'Edinson Cavani', 
-                    total: 74, img: 'https://i.pinimg.com/originals/e9/ea/79/e9ea796f73d6e1b503dae131d5ef17c1.jpg' })
+                this.stickers.push({ number: 50, total: 11 })
+                this.stickers.push({ number: 51, total: 15 })
+                this.stickers.push({ number: 52, total: 81 })
+                this.stickers.push({ number: 53, total: 61 })
+                this.stickers.push({ number: 54, total: 31 })
+                this.stickers.push({ number: 61, total: 25 })
+                this.stickers.push({ number: 62, total: 66 })
+                this.stickers.push({ number: 69, total: 74 })
             }
         }
     },
